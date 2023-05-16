dc50fb341730ddc130e246518f9ced3e2d59fe5c
bradleyboyuyang <119020065@link.cuhk.edu.cn>
2023-05-17 01:59:59 +0800

    finish half of 1.5.4

--- Level 1/Section 1.5/Exercise 4/Exercise 4.cpp
+++ Level 1/Section 1.5/Exercise 4/Exercise 4.cpp
@@ -3,12 +3,51 @@
 #include <algorithm>
 
 
+template <typename T>
+struct IndexSolver {
+	static std::size_t findIndex(std::vector<T>& sortedVec, T& x) {
+
+		// we can use STL algorithm `find_if`
+		auto idx = std::find_if(sortedVec.begin(), sortedVec.end(), [=](T& d) {return d > x; });
+
+		// Alternative: STL algorithm lower_bound or upper_bound
+		// For specification v[i] <= x < v[i + 1], we should use STL `upper_bound`
+		// if it is v[i] < x <= v[i + 1], we use STL `lower_bound`
+		//auto idx = std::upper_bound(sortedVec.begin(), sortedVec.end(), x);
+
+		// The output index should be decreased by 1 
+		if (idx != sortedVec.end()) {
+			std::size_t index = std::distance(sortedVec.begin(), idx);
+			return index - 1;
+		}
+		else {
+			std::cout << "Index out of range" << std::endl;
+			return static_cast<std::size_t>(0);
+		}
+	}
+};
+
+template <typename T>
+struct Comparator {
+	static 
+};
+
+
+
+
 int main()
 {
 	/// Part (A) Given a sorted numeric vector v and a target value x, find the first index i such that 
 	/// v[i] <= x < v[i + 1]. Determine the return type.
-
-
+	std::vector<int> vec(10);
+	for (int i = 0; i < 10; ++i) {
+		vec[i] = i * 2 + 5;
+	}
+	std::cout << "Original Vector: ";
+	for (auto val : vec) { std::cout << val << ", "; };
+	std::cout << std::endl;
+	int x{ 8 };
+	std::cout << "Finding value: " << x << " Result Index: " << IndexSolver<int>::findIndex(vec, x) << std::endl;
 
 
 	/// Part (B) Find the maximum error between two vectors v1 and v2 in a given index range [i; j]
---
+++ Level 1/Section 1.5/Exercise 5/Exercise 5.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using ShapePointer = std::shared_ptr<Shape>;
+using IODevicePointer = std::shared_ptr<IODevice>;
+
+// Class hierarchy
+class Shape
+{
+public:
+	virtual void display(IODevice& ioDevice) const = 0;
+};
+
+class Circle : Shape
+{
+public:
+	void display(IODevice& ioDevice) {
+
+	}
+};
+
+class Line : Shape
+{
+
+};
+
+
+
+class IODevice
+{ // Interface for displaying CAD objects
+public:
+	virtual void operator << (const Circle& c) = 0;
+	virtual void operator << (const Line& c) = 0;
+};
+
+class Builder
+{ // A Builder hierarchy that builds shapes and io devices
+public:
+	std::tuple<ShapePointer, IODevicePointer> getProduct()
+	{ // GOF (!) Template Method pattern
+	// TBD
+	}
+	// Hook functions that derived classes must implement
+	virtual ShapePointer getShape() = 0;
+	virtual IODevicePointer getIODevice() = 0;
+};
+
+
+
+int main()
+{
+	/// Create derived classes Circle and Line. Create I/O device classes to display CAD shapes in different ways.
+
+
+
+
+	/// Part (B) Create and test a number of builders and apply them in the current context
+
+
+
+}
+
