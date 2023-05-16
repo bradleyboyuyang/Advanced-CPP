20bd192469c9686993a8f334f4de59fbcd90e1f8
bradleyboyuyang <119020065@link.cuhk.edu.cn>
2023-05-16 20:38:21 +0800

    finish 1.6.4

---
+++ Level 1/Section 1.5/Exercise 4/Exercise 4.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+int main()
+{
+	/// Part (A) Given a sorted numeric vector v and a target value x, find the first index i such that 
+	/// v[i] <= x < v[i + 1]. Determine the return type.
+
+
+
+
+	/// Part (B) Find the maximum error between two vectors v1 and v2 in a given index range [i; j]
+	/// We wish to compute the difference in some(customisable) norm, specifically the absolute error, relative error and
+	/// the index values where these errors occur.
+
+	// Ideally, you should use the following C++ functionality if possible:
+	//• Move semantics instead of copy constructors.
+	//• Smart pointers(if applicable).
+	//• Function objectsand lambda functions.
+	//• Use as much of STL Algorithms as you can(investigate what’s in the library)
+
+
+}
+
---
+++ Level 1/Section 1.6/Exercise 4/Exercise 4.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <list>
+
+// C++ 98 approach
+// Data storage types
+template <typename T> struct Storage
+{
+    // Possible ADTs and their memory allocators
+    // typedef std::vector<T, CustomAllocator<T>> type;
+    // typedef std::vector<T, std::allocator<T>> type;
+    typedef std::list<T, std::allocator<T>> type;
+};
+
+
+template <typename T>
+class Client
+{ // An example of Composition
+private:
+    typename Storage<T>::type data; // typename mandatory
+public:
+    Client(int n, const T& val) : data(n, val) {}
+    void print() const
+    {
+        std::for_each(data.begin(), data.end(), [](const T& n)
+            { std::cout << n << ","; });
+        std::cout << '\n';
+    }
+};
+
+
+/// Part (A) (re)Create the class using alias template instead of typedef.
+template <typename T> 
+using Type = std::list<T, std::allocator<T>>;
+
+template <typename T> class ClientNew {
+private:
+    Type<T> data;
+public:
+    ClientNew(int n, const T& val) : data(n, val) {}
+    void print() const
+    {
+        std::for_each(data.begin(), data.end(), [](const T& n)
+            { std::cout << n << ","; });
+        std::cout << '\n';
+    }
+};
+
+int main()
+{
+
+    /// Part (B) Test your code. Do you get the same output as before?
+
+    // Client of storage using typedef (C++ 98)
+    int n = 10; int val = 2;
+    Client<int> myClient(n, val); myClient.print();
+
+    // Client of storage using alias template
+    ClientNew<int> myClient2(n, val); myClient2.print();
+
+    // Yes, we have the same output with much simpler code structure with C++ 11 `using`, rather than C++ 98 `typedef`。
+}
+
+
+
