7e7cae48cbe27c26969c3bd326fa2ff4a212d89f
bradleyboyuyang <119020065@link.cuhk.edu.cn>
2023-06-04 20:45:03 +0800

    update

---
+++ Level 2/Section 2.2/Exercise 3/Exercise 3.cpp
@@ -0,0 +1,77 @@
+﻿/*
+    Level 2.2: Advanced Type Traits
+    Problem 3: Conversions
+
+    Name: Boyu Yang
+
+    Problem objective:
+    Sometimes you may wish to structurally change fundamental properties of types in an application.
+    Examples are:
+    • Mapping integers to unsigned integers and vice versa.
+    • Add/remove the const specifier to or from a type.
+    • Add/remove a pointer to or from a type.
+    • Add/remove the volatile specifier to or from a type.
+    • Discuss the difference between direct list initialisation and copy list initialisation in C++17/20
+    compared to C++11. In which case does auto deduction reduce to std::initializer_list?
+    Give a code example. Under which circumstances can we get ill-formed expressions?
+    • Discuss how Class Template Argument Deduction (CTAD)) reduces code verbosity. Give a non-trivial
+    example using std::tuple and a user-defined class.
+    • Investigate the applicability of decltype and std::is_same when comparing the type of a result with a
+    “target” type.
+    Answer the following questions:
+    a) Write a separate function for each of the above requirements.
+    b) Test the functions on a range of fundamental, scalar, object, arithmetic and compound types.
+
+*/
+
+#include <iostream>
+#include <vector>
+#include <array>
+#include <algorithm>
+
+
+
+int main()
+{
+    /// Part (A) 
+    /// Part (B) Test the functions on a range of fundamental, scalar, object, arithmetic and compound types
+    // fundamental types
+
+    unsigned int a = 123;
+    signed int b = -123;
+    std::cout << "Signed to unsigned: " << mapInteger()
+        std::cout << mapInteger(b);
+
+    //std::cout << "Fundamental types: " << std::endl;
+    //std::cout << "void: " << std::is_array<void>::value << std::endl;
+    //std::cout << "int: " << std::is_array<int>::value << std::endl;
+    //std::cout << "nullptr_t: " << std::is_array<std::nullptr_t>::value << std::endl;
+
+    //// scalar types
+    //std::cout << "Scalar types: " << std::endl;
+    //enum Suit { SPADE, HEART, DIAMOND, CLUB };
+    //std::cout << "double: " << std::is_array<double>::value << std::endl;
+    //std::cout << "enum: " << std::is_array<Suit>::value << std::endl;
+    //std::cout << "nullptr: " << std::is_array<decltype(nullptr)>::value << std::endl;
+
+    //// arithmetic types
+    //std::cout << "Arithmetic types: " << std::endl;
+    //std::cout << "bool: " << std::is_array<bool>::value << std::endl;
+    //std::cout << "float: " << std::is_array<float>::value << std::endl;
+    //std::cout << "char const: " << std::is_array<char const>::value << std::endl;
+
+    //// object types
+    //std::cout << "Object types: " << std::endl;
+    //std::cout << "string: " << std::is_array<std::string>::value << std::endl;
+    //std::cout << "int pointer: " << std::is_array<int*>::value << std::endl;
+    //std::cout << "array: " << std::is_array<float[5]>::value << std::endl;
+
+    //// compound types
+    //std::cout << "Compound types: " << std::endl;
+    //std::cout << "vector: " << std::is_array<std::vector<int>>::value << std::endl;
+    //std::cout << "int reference: " << std::is_array<int&>::value << std::endl;
+    //std::cout << "int pointer: " << std::is_array<int*>::value << std::endl;
+    //std::cout << "array pointer: " << std::is_array<std::array<int, 2>*>::value << std::endl;
+    //std::cout << "array reference: " << std::is_array<std::array<int, 2>&>::value << std::endl;
+    //std::cout << "high dimention array: " << std::is_array<int[3][5][4]>::value << std::endl;
+}
--- Level 2/Section 2.5 2.6/Exercise 3/Exercise 3.cpp
+++ Level 2/Section 2.5 2.6/Exercise 3/Exercise 3.cpp
@@ -1,79 +1,71 @@
 ﻿/*
-    Level 2.2: Advanced Type Traits
-    Problem 3: Conversions
+    Level 2.5, 2.6: IEEE 754 and C Floating Point Classification
+    Problem 3: Machine Precision Issues
 
     Name: Boyu Yang
 
     Problem objective:
-    Sometimes you may wish to structurally change fundamental properties of types in an application.
-    Examples are:
-    • Mapping integers to unsigned integers and vice versa.
-    • Add/remove the const specifier to or from a type.
-    • Add/remove a pointer to or from a type.
-    • Add/remove the volatile specifier to or from a type.
-    • Discuss the difference between direct list initialisation and copy list initialisation in C++17/20
-    compared to C++11. In which case does auto deduction reduce to std::initializer_list?
-    Give a code example. Under which circumstances can we get ill-formed expressions?
-    • Discuss how Class Template Argument Deduction (CTAD)) reduces code verbosity. Give a non-trivial
-    example using std::tuple and a user-defined class.
-    • Investigate the applicability of decltype and std::is_same when comparing the type of a result with a
-    “target” type.
-    Answer the following questions:
-    a) Write a separate function for each of the above requirements.
-    b) Test the functions on a range of fundamental, scalar, object, arithmetic and compound types.
+    This exercise requires that you do some research into how C++ 11 and Boost C++ Math Toolkit supports
+    the following functionality (x is a given value):
+    • The next representable value that is greater than x. An overflow_error is thrown if no such value
+    exists (float_next).
+    • The next representable value that is less than x. An overflow_error is thrown if no such value exists
+    (float_prior).
+    • Advance a floating-point number by a specified number of ULP (Unit in Last Place) (float_advance).
+    • Find the number of gaps/bits/ULP between two floating-point value (float_distance).
+    • (C++11 and Boost) Return the next representable value of x in the direction y
+    (std/boost::nextafter(x,y)).
 
 */
 
 #include <iostream>
-#include <vector>
-#include <array>
-#include <algorithm>
+#include <cmath>
+#include <boost/math/special_functions/next.hpp>
+
+int main() {
+    /// Check how C++ 11 and Boost C++ Math Toolkit supports the following functionality
+    /// The next representable value that is greater than x. An overflow_error is thrown if no such value exists(float_next)
+    // Boost uses boost::math::float_next; STL use nextafter to get the same result
+    float f1 = 1e-20;
+    std::cout << "(Boost) Next representable value that is greater than " << f1 << ": " << boost::math::float_next(f1) << std::endl;
+    std::cout << "(STL) Next representable value that is greater than " << f1 << ": " << std::nextafter(f1, std::numeric_limits<float>::infinity()) << std::endl;
+
+    std::cout << "(Boost) Next representable value that is greater than STL minimum" << ": " << boost::math::float_next(std::numeric_limits<float>::min()) << std::endl;
+    std::cout << "(STL) Next representable value that is greater than STL minimum" << ": " << std::nextafter(std::numeric_limits<float>::min(), std::numeric_limits<float>::infinity()) << std::endl;
+
+    /// The next representable value that is less than x. An overflow_error is thrown if no such value exists (float_prior).
+    // Boost uses boost::math::float_next; STL can still use nextafter to get the same result, with a negative sign
+    std::cout << "(Boost) Next representable value that is less than " << f1 << ": " << boost::math::float_prior(f1) << std::endl;
+    std::cout << "(STL) Next representable value that is less than " << f1 << ": " << std::nextafter(f1, -std::numeric_limits<float>::infinity()) << std::endl;
+
+    std::cout << "(Boost) Next representable value that is less than STL maximum" << ": " << boost::math::float_prior(std::numeric_limits<float>::max()) << std::endl;
+    std::cout << "(STL) Next representable value that is less than STL maximum" << ": " << std::nextafter(std::numeric_limits<float>::max(), -std::numeric_limits<float>::infinity()) << std::endl;
 
 
+    /// Advance a floating-point number by a specified number of ULP (Unit in Last Place) (float_advance).
+    // Use STL algorithms: repeatedly use nextafter to achieve the specified number of ULP
+    float z = 1.1;
+    int ulp = 200; // number of ULP
+    int ulp2 = -30000;
+    // Use boost::math::float_advance
+    std::cout << "Advance " << z << " by " << ulp << " ULP: " << boost::math::float_advance(z, ulp) << std::endl;
+    std::cout << "Advance " << z << " by " << -ulp << " ULP: " << boost::math::float_advance(z, -ulp) << std::endl;
+    std::cout << "Advance " << z << " by " << ulp2 << " ULP: " << boost::math::float_advance(z, ulp2) << std::endl;
+    std::cout << "Advance " << z << " by " << -ulp2 << " ULP: " << boost::math::float_advance(z, -ulp2) << std::endl;
 
-int main()
-{
-    /// Part (A) 
-    /// Part (B) Test the functions on a range of fundamental, scalar, object, arithmetic and compound types
-    // fundamental types
+    /// Find the number of gaps/bits/ULP between two floating-point value (float_distance).
+    // Number gaps/bits/ULP between 2 floating-point values a and b
+    // Returns a signed value indicating whether a < b
+    double a = 0.1; double b = a + std::numeric_limits<float>::min();
+    std::cout << boost::math::float_distance(a, b) << std::endl;
+    std::cout << boost::math::float_distance(1.0, 0.0) << std::endl;
+    std::cout << boost::math::float_distance(0.0, 0.5) << std::endl;
 
-    unsigned int a = 123;
-    signed int b = -123;
-    std::cout << "Signed to unsigned: " << mapInteger()
-        std::cout << mapInteger(b);
+    /// (C++11 and Boost) Return the next representable value of x in the direction y (std / boost::nextafter(x, y)).
+    float f = 1.111;
+    float next_stl = std::nextafter(f, std::numeric_limits<float>::max());
+    std::cout << "(STL) Next representable value greater than " << f << ": " << next_stl << std::endl;
+    float next_boost = boost::math::nextafter(f, std::numeric_limits<float>::max());
+    std::cout << "(Boost) Next representable value greater than " << f << ": " << next_boost << std::endl;
 
-    //std::cout << "Fundamental types: " << std::endl;
-    //std::cout << "void: " << std::is_array<void>::value << std::endl;
-    //std::cout << "int: " << std::is_array<int>::value << std::endl;
-    //std::cout << "nullptr_t: " << std::is_array<std::nullptr_t>::value << std::endl;
-
-    //// scalar types
-    //std::cout << "Scalar types: " << std::endl;
-    //enum Suit { SPADE, HEART, DIAMOND, CLUB };
-    //std::cout << "double: " << std::is_array<double>::value << std::endl;
-    //std::cout << "enum: " << std::is_array<Suit>::value << std::endl;
-    //std::cout << "nullptr: " << std::is_array<decltype(nullptr)>::value << std::endl;
-
-    //// arithmetic types
-    //std::cout << "Arithmetic types: " << std::endl;
-    //std::cout << "bool: " << std::is_array<bool>::value << std::endl;
-    //std::cout << "float: " << std::is_array<float>::value << std::endl;
-    //std::cout << "char const: " << std::is_array<char const>::value << std::endl;
-
-    //// object types
-    //std::cout << "Object types: " << std::endl;
-    //std::cout << "string: " << std::is_array<std::string>::value << std::endl;
-    //std::cout << "int pointer: " << std::is_array<int*>::value << std::endl;
-    //std::cout << "array: " << std::is_array<float[5]>::value << std::endl;
-
-    //// compound types
-    //std::cout << "Compound types: " << std::endl;
-    //std::cout << "vector: " << std::is_array<std::vector<int>>::value << std::endl;
-    //std::cout << "int reference: " << std::is_array<int&>::value << std::endl;
-    //std::cout << "int pointer: " << std::is_array<int*>::value << std::endl;
-    //std::cout << "array pointer: " << std::is_array<std::array<int, 2>*>::value << std::endl;
-    //std::cout << "array reference: " << std::is_array<std::array<int, 2>&>::value << std::endl;
-    //std::cout << "high dimention array: " << std::is_array<int[3][5][4]>::value << std::endl;
 }
-
-
