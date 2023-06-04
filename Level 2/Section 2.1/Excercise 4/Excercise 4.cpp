1d64b08bcff03fd1f922f5829ae62dfdb5413773
bradleyboyuyang <119020065@link.cuhk.edu.cn>
2023-06-04 10:48:05 +0800

    finish 2.1.4

--- Level 2/Section 2.1/Excercise 4/Excercise 4.cpp
+++ Level 2/Section 2.1/Excercise 4/Excercise 4.cpp
@@ -19,7 +19,6 @@
 #include <iostream>
 #include <type_traits>
 
-
 class Shape {
 public:
     virtual void draw() = 0;
@@ -28,84 +27,53 @@ public:
 class Point : public Shape
 {
 public:
-    Point() {}
+    // default constructor
+    Point() {};
+
+    /// Part (D), set copy constructor and assignment operator as "deleted"
+    // copy constructor
+    //Point(Point& p) = delete;
+    
+    // assignment operator
+    //Point& operator = (const Point& p) = delete;
+
+    /// Part (E), add explicit move constructor and move assignment operator
+    // explicit move constructor
+    //Point(Point&& p) {};
+    
+    // move assignment operator
+    //Point& operator = (const Point&& p) {};
+
     virtual void draw() override {}
 };
 
-template <typename T>
-void MethodInformation(const T& t)
-{
-    // Numeric type 
-    std::cout << "Default constructible? " << std::boolalpha << std::is_default_constructible<T>::value << std::endl;
-    std::cout << "Copy constructible? " << std::boolalpha << std::is_copy_constructible<T>::value << std::endl;
-    std::cout << "Move constructible? " << std::boolalpha << std::is_move_constructible<T>::value << std::endl;
-    std::cout << "Copy assignable? " << std::boolalpha << std::is_copy_assignable<T>::value << std::endl;
-    std::cout << "Move assignable? " << std::boolalpha << std::is_move_assignable<T>::value << std::endl;
-}
 
 int main()
 {
     Shape* shape;
     Point* point;
     /// Part (A) Programmatically determine if type Point has a virtual destructor.
-    std::cout << "*** Has Virtual Destructor? : **** \n";
-    std::cout << "Point: " << std::boolalpha << std::has_virtual_destructor<Point>::value << std::endl;
-    
+    std::cout << "Has Virtual Destructor? " << std::boolalpha << std::has_virtual_destructor<Point>::value << std::endl; // false
+
     /// Part (B) Programmatically determine if type Point has the following constructors : default, copy, move.
+    std::cout << "Default constructible? " << std::boolalpha << std::is_default_constructible<Point>::value << std::endl; // true
+    std::cout << "Copy constructible? " << std::boolalpha << std::is_copy_constructible<Point>::value << std::endl; // true
+    std::cout << "Move constructible? " << std::boolalpha << std::is_move_constructible<Point>::value << std::endl; // true
+
     /// Part (C) Programmatically determine if type Point is copy assignable / move assignable.
-    // We answer the two questions using the defined 
+    std::cout << "Copy assignable? " << std::boolalpha << std::is_copy_assignable<Point>::value << std::endl; // true
+    std::cout << "Move assignable? " << std::boolalpha << std::is_move_assignable<Point>::value << std::endl; // true
 
-
-    /// Part (D) Set the copy constructorand assignment operator as ‘deleted functions’and perform steps b) - c) again;
-    /// do you get the same result ?
-
+    /// Part (D) Set the copy constructor and assignment operator as ‘deleted functions’ and perform steps b) - c) again;
+    /// do you get the same result?
+    // Uncomment the code in the class under "Part (D)" we can get:
+    // The type Point is default constructible, but not copy constructible, move constructible, copy assignable, nor move assignable.
 
     /// Part (E) Add move semantics code(explicit move constructor and move assignment operator) and perform
     /// steps b) - c) again; do you get the same result ?
-     
+    // Uncomment the code in the class under "Part (E)" we can get:
+    // The type Point is default constructible, move constructible, move assignable, but not copy constructible and copy assignable.
 
-
-    // b) Programmatically determine if type Point has default constructor, move, copy .
-    std::cout << "\n*** Default constructor? : **** \n";
-    std::cout << "Point: " << std::boolalpha << std::is_default_constructible<Point>::value << std::endl;
-    // result is True
-
-    std::cout << "\n*** Default move? : **** \n";
-    std::cout << "Point: " << std::boolalpha << std::is_move_constructible<Point>::value << std::endl;
-    // result is True 
-
-    std::cout << "\n*** Default copy? : **** \n";
-    std::cout << "Point: " << std::boolalpha << std::is_copy_constructible<Point>::value << std::endl;
-    // result is True
-
-    // c) Programmatically determine if type Point is copy assignable/move assignable.
-    std::cout << "\n*** Copy assignable? : **** \n";
-    std::cout << "Point: " << std::boolalpha << std::is_copy_assignable<Point>::value << std::endl;
-    // result is True
-
-    std::cout << "\n*** move assignable? : **** \n";
-    std::cout << "Point: " << std::boolalpha << std::is_move_assignable<Point>::value << std::endl;
-    // result is True
-
-    // d) Set the copy constructor and assignment operator as �deleted functions� and perform steps b)-c) again; do you get the same result?
-    // Just re-ran program above and the following results were found:
-
-    //      default destructor --> False (this is the same)
-    //      default constructor --> False (this is different from the original)
-    //      default move --> False (this is different from the original)
-    //      default copy --> False (this is different from the original)
-    //      copy assignable --> False (this is different from the original)
-    //      move assignable --> False (this is different from the original)
-
-    // e) Add move semantics code(explicit move constructor and move assignment operator)
-    //    and perform steps b) - c) again; do you get the same result ?
-
-    //      virtual destructor -->  False (this is the same as the original)
-    //      default constructor --> True (this is the same as the original)
-    //      default move --> True (this is the same as the original)
-    //      default copy --> False (this is different from the original)
-    //      copy assignable --> True (this is the different from before)
-    //      move assignable --> True (this is the different from before)
 }
 
 
