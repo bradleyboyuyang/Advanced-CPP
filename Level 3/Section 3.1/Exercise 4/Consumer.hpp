2c73191f1a5168aa679bd4901289034463c1c3e1
bradleyboyuyang <119020065@link.cuhk.edu.cn>
2023-06-17 15:46:07 +0800

    finish 3.1.4

---
+++ Level 3/Section 3.1/Exercise 4/Consumer.hpp
@@ -0,0 +1,38 @@
+/*
+    Level 3.1: Threads
+    Problem 4: Synchronising Queue
+    Name: Boyu Yang
+
+    File: Consumer.hpp
+    Description: Implement the consumer class
+
+*/
+
+#pragma once
+#include <iostream>
+#include <string>
+#include <thread>
+#include "SyncQueue.hpp"
+
+// Class that consumes objects from a queue
+class Consumer
+{
+private:
+    int m_id; // The id of the consumer
+    SynchronisedQueue<std::string>* m_queue; // The queue to use
+public:
+    // Constructor with id and the queue to use
+    Consumer(int id, SynchronisedQueue<std::string>* queue)
+    {
+        m_id = id; m_queue = queue;
+    }
+    // The thread function reads data from the queue
+    void operator () ()
+    {
+        while (true)
+        {
+            // Get the data from the queue and print it
+            std::cout << "Dequeue: Consumer " << std::to_string(m_id).c_str() << " consumed (" << m_queue->Dequeue().c_str() << ")" << std::endl;
+        }
+    }
+};
---
+++ Level 3/Section 3.1/Exercise 4/Exercise 4.cpp
@@ -0,0 +1,51 @@
+﻿/*
+    Level 3.1: Threads
+    Problem 4: Synchronising Queue
+
+    Name: Boyu Yang
+
+    File: Exercise 4.cpp
+    Description: Main file for execution
+    
+    Problem objective:
+    We implement a synchronising (thread-safe) FIFO queue in C++11. In this exercise we create our own
+    generic synchronising queue having an embedded STL queue.
+    a) Implement this template class, in particular its template parameters and functions for queueing and dequeing.
+    b) Add locking mechanisms to functions for synchronisation effects.
+    c) Add notification mechanisms to allow thread notification (use condition variables for notification).
+    d) Test the queue by creating producer and consumer threads.
+
+*/
+
+#include <boost/thread.hpp>
+
+#include "Consumer.hpp"
+#include "Producer.hpp"
+
+
+int main() {
+    /// d) Test the queue by creating producer and consumer threads.
+        // The number of producers/consumers
+    int nrProducers = 3, nrConsumers = 3;
+    // The shared queue
+    SynchronisedQueue<std::string> queue;
+    // Create producers
+    boost::thread_group producers;
+    for (int i = 0; i < nrProducers; i++)
+    {
+        Producer p(i, &queue);
+        producers.create_thread(p);
+    }
+    // Create consumers
+    boost::thread_group consumers;
+    for (int i = 0; i < nrConsumers; i++)
+    {
+        Consumer c(i, &queue);
+        consumers.create_thread(c);
+    }
+    // Wait for enter
+    getchar();
+    // Interrupt the threads and stop them
+    producers.interrupt_all(); producers.join_all();
+    consumers.interrupt_all(); consumers.join_all();
+}
